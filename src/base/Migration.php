e16b9f354a897cbd3ebf564cff453a3648730a56
Roman Zhuravlev <zhuravljov@gmail.com>
2018-09-04 08:29:02 +0300

    DB schema refactoring

--- src/base/Migration.php
+++ src/base/Migration.php
@@ -30,4 +30,26 @@ abstract class Migration extends \yii\db\Migration
         $this->env = $env;
         parent::__construct($config);
     }
+
+    /**
+     * @inheritdoc
+     */
+    public function binary($length = null)
+    {
+        if ($this->db->driverName === 'mysql') {
+            return $this->db->schema->createColumnSchemaBuilder('longblob');
+        }
+        return parent::binary($length);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function text()
+    {
+        if ($this->db->driverName === 'mysql') {
+            return $this->db->schema->createColumnSchemaBuilder('longtext');
+        }
+        return parent::text();
+    }
 }
